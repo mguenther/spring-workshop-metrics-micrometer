3c2b39f0853c7fe408646dbda3e72b1165e3650f
Markus Günther <markus.guenther@gmail.com>
2023-05-13 11:08:36 +0200

    Adds the solution to task #4

--- src/main/java/workshop/spring/metrics/ShoppingCartResource.java
+++ src/main/java/workshop/spring/metrics/ShoppingCartResource.java
@@ -1,5 +1,7 @@
 package workshop.spring.metrics;
 
+import io.micrometer.core.instrument.DistributionSummary;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,35 +13,74 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import workshop.spring.metrics.util.StopWatch;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class ShoppingCartResource {
 
     private final ShoppingCart cart;
 
+    private final DistributionSummary serviceTimes;
+
     @Autowired
-    public ShoppingCartResource(final ShoppingCart cart) {
+    public ShoppingCartResource(final ShoppingCart cart, final MeterRegistry meterRegistry) {
         this.cart = cart;
+        this.serviceTimes = meterRegistry.summary("service-times");
     }
 
     @GetMapping(path = "/cart", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Items> getItems() {
-        return ResponseEntity.ok(cart.getItems());
+        final var timer = StopWatch.time();
+        try {
+            delay();
+            return ResponseEntity.ok(cart.getItems());
+        } finally {
+            serviceTimes.record(timer.elapsedInMillis());
+        }
     }
 
     @PutMapping(path = "/cart/{productId}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Items> addItem(@PathVariable("productId") final String productId, @RequestBody final Item item) {
-        return ResponseEntity.ok(cart.addItem(item));
+        final var timer = StopWatch.time();
+        try {
+            delay();
+            return ResponseEntity.ok(cart.addItem(item));
+        } finally {
+            serviceTimes.record(timer.elapsedInMillis());
+        }
     }
 
     @DeleteMapping(path = "/cart/{productId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Items> removeItem(@PathVariable("productId") final String productId) {
-        return ResponseEntity.ok(cart.removeItem(productId));
+        final var timer = StopWatch.time();
+        try {
+            delay();
+            return ResponseEntity.ok(cart.removeItem(productId));
+        } finally {
+            serviceTimes.record(timer.elapsedInMillis());
+        }
     }
 
     @PostMapping(path = "/cart", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Items> completeOrder() {
-        cart.closeOrder();
-        return ResponseEntity.status(HttpStatus.OK).build();
+        final var timer = StopWatch.time();
+        try {
+            cart.closeOrder();
+            return ResponseEntity.status(HttpStatus.OK).build();
+        } finally {
+            serviceTimes.record(timer.elapsedInMillis());
+        }
+    }
+
+    private void delay() {
+        try {
+            final int delay = (int) (Math.random() * 1000) + 500; // introduce a delay from [500;1500) milliseconds
+            TimeUnit.MILLISECONDS.sleep(delay);
+        } catch (InterruptedException e) {
+            // ignore
+            e.printStackTrace();
+        }
     }
 }
